e7b37622d909cd11fed47162cba3edb0d4ae1d33
Rithvik <sairithvik.achutuni@students.iiit.ac.in>
2025-08-10 00:25:59 +0530

    experimented with theme

---
+++ lua/custom/plugins/techbase-theme.lua
@@ -0,0 +1,9 @@
+return {
+  { -- You can easily change to a different colorscheme.
+    'mcauley-penney/techbase.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- config = function()
+    --   vim.cmd 'colorscheme techbase'
+    -- end,
+  },
+}
